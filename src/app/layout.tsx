b62cb362bf90af2896338f4c72490740eb59989b
arkochan <najmussakib@iut-dhaka.edu>
2023-06-25 08:44:01 +0000

    previos layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-screen bg-black">
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-import { Montserrat } from "next/font/google";
+"use client";
+import { Content, Montserrat } from "next/font/google";
+import { ChangeEvent, useState } from "react";
+//import { getUsersTasks } from apiCalls;
+import { User } from "@prisma/client";
+import { check } from "prettier";
+import { randomInt, randomUUID } from "crypto";
 
 // If loading a variable font, you don't need to specify the font weight
 const montserrat = Montserrat({
@@ -7,18 +13,100 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
-const Home = () => {
+interface Task {
+  id: string;
+  content: string;
+  checked: boolean;
+}
+
+function getUsersTasks(userId: string): Task[] {
+  //do db
+  // do api
+  return [
+    {
+      id: "2hj13ghjg3",
+      content: "Buy Cow",
+      checked: false,
+    },
+    {
+      id: "346jkh54k3j",
+      content: "Send Email",
+      checked: true,
+    },
+    {
+      id: "1227836k3j",
+      content: "Call Aunty",
+      checked: false,
+    },
+  ];
+}
+
+async function Home() {
+  const [currentUser, setCurrentUser] = useState();
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [formTask, setformTask] = useState<Task>({
+    id: randomInt.toString() as string,
+    content: "",
+    checked: false,
+  });
+  function handleAddTask() {
+    if (formTask.content === "") return;
+    setTasks((prevTasks) => [...prevTasks, formTask]);
+
+    setformTask({
+      id: randomInt.toString() as string,
+      content: "",
+      checked: false,
+    });
+  }
+  function addTaskFeildChange(event: ChangeEvent<HTMLInputElement>) {
+    setformTask((prevTask) => ({
+      ...prevTask,
+      content: event.target.value,
+    }));
+  }
+  function toggleCheckbox(id: string) {
+    setTasks((prevTask) =>
+      prevTask.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task
+      )
+    );
+  }
+
   return (
-    <div
-      className={` ${montserrat.variable} font-montserrat flex h-screen justify-center bg-black `}
-    >
-      <div className="text-center">
-        <h1 className="font-bold text-white">Yatada!</h1>
+    <>
+      <div className="flex flex-col items-center justify-center p-10">
+        <div>Add a task:</div>
+        <div>Tasks in hand:</div>
 
-        <p className=" text-white">Yet Another To (and) Do App</p>
+        <div className="p-2">
+          {tasks?.map((task) => (
+            <div key={task.id} className="flex items-center space-x-2 p-1">
+              <input
+                type="checkbox"
+                checked={task.checked}
+                onChange={() => toggleCheckbox(task.id)}
+                className="checkbox"
+              />
+              <div>{task.content}</div>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-4 flex items-center space-x-2">
+          <input
+            type="text"
+            placeholder="Add a task"
+            className="input-bordered input-primary input input-sm w-full max-w-xs"
+            onChange={addTaskFeildChange}
+            value={formTask.content}
+          />
+          <button className="btn-success btn-sm btn" onClick={handleAddTask}>
+            Add
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
-};
-
+}
 export default Home;
